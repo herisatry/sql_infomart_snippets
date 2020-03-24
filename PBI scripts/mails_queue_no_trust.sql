257c4c359c230e2655b042a9cd8c30c554f04260
herisatry <herisatry@gmail.com>
2020-03-24 22:54:26 +0200

    fcr and mail queues

--- PBI scripts/mails_queue_no_trust.sql
+++ PBI scripts/mails_queue_no_trust.sql
@@ -1,5 +1,7 @@
 USE INTERACTIONS
 
-SELECT lang as [lang], count(*) as [Qty] , min(moved_to_queue_at) AS [oldest email date]
+SELECT i0.lang as [lang], count(*) as [Qty] , MIN(i0.moved_to_queue_at) AS [oldest email]
 
-FROM interactions where queue = 'BasicEmailInbound.default.EmailInboundContactProcessed'  AND destinations is NULL AND ( customer_info  NOT LIKE '%trust%' OR ext_type NOT LIKE '%trust%') group by lang
+FROM interactions i0
+
+where i0.queue = 'BasicEmailInbound.default.EmailInboundContactProcessed'  AND i0.destinations is NULL AND ( i0.customer_info  NOT LIKE '%trust%' OR i0.ext_type NOT LIKE '%trust%' ) group by i0.lang
